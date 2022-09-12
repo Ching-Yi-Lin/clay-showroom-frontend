66f3291adc16cf10a8717b2292a48265f4bee7d1
Carol <lovellcarol@hotmail.com>
2022-09-12 16:49:21 +1200

    Add signup page

--- pages/signup.js
+++ pages/signup.js
@@ -7,7 +7,14 @@ export default function Home({ }) {
       <Head>
         <title>Sign Up | Clay Showroom</title>
       </Head>
-      <section className="sign-up">Sign Up Page</section>
+      {/* <section className="sign-up">Sign Up Page</section>
+      <form action="/send-data-here" method="post">
+  <label for="first">First name:</label>
+  <input type="text" id="first" name="first" />
+  <label for="last">Last name:</label>
+  <input type="text" id="last" name="last" />
+  <button type="submit">Submit</button>
+</form> */}
 
       <style jsx>
         {`
